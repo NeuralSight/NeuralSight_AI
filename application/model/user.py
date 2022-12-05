c794350bbda6d2351b4ea54c448415fded219d17
root <>
2022-12-05 14:58:12 +0300

    updated models objects

--- application/model/user.py
+++ application/model/user.py
@@ -11,19 +11,3 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean(), default=True)
     is_superuser = Column(Boolean(), default=False)
-
-# class Patient(Base):
-#     __tablename__ = 'patient'
-    # id---2 (db_id and docker_id. report-->string)
-
-
-    # get id later and updated report..
-
-
-### Diagonistic Report...
-# - after inference..
-#     - string, doctor,
-#
-# ### Save patient Details.
-#     - Patient object.-> upload image.
-#     - create patient()
