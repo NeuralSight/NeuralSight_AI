44b80c9a10448f3e92c20934ed9284d120b0b0df
root <>
2022-12-12 18:50:35 +0300

    updated User-> Patient relations

--- application/model/user.py
+++ application/model/user.py
@@ -1,7 +1,7 @@
 from db.db_base import Base
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
-
+from .inference import Patient, Report
 
 class User(Base):
     __tablename__ = 'user'
@@ -11,6 +11,7 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean(), default=True)
     is_superuser = Column(Boolean(), default=False)
+    patient_records = relationship("Patient", back_populates="user")
 
 # class Patient(Base):
 #     __tablename__ = 'patient'
