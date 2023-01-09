d53fcd0696f6bc799ea2a36c91482f811c6fcc23
root <>
2023-01-09 11:12:01 +0300

    updated endpoint for user delete

--- application/db/database.py
+++ application/db/database.py
@@ -5,16 +5,26 @@ import os
 import dotenv
 
 
+# psql \
+#    --host=nsight.cftp73dgheul.us-east-1.rds.amazonaws.com \
+#    --port=5432 \
+#    --username=nsight \
+#    --dbname=nsight
+#    --password=neurallabs \
+
+
 dotenv.load_dotenv()
 
 if not os.getenv("PRODUCTION"):
-    SQLALCHEMY_DATABASE_URL = "sqlite:///./tester.db"
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./neurallabs.db"
     engine = create_engine(
         SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
     )
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 else:
-    # SQLALCHEMY_DATABASE_URL = "postgresql://nsight:nsight@localhost/nsight"
-    SQLALCHEMY_DATABASE_URL = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}/{os.getenv("DB_NAME")}'
+    # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/nsight"
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./neurallabs.db"
+    # SQLALCHEMY_DATABASE_URL = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
+    print(f"DB URL  {SQLALCHEMY_DATABASE_URL}")
     engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
