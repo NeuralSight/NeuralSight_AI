81be8dc883fb20485f3bc598271c065eca199198
root <>
2023-01-09 12:58:06 +0300

    updated super user

--- application/app.py
+++ application/app.py
@@ -47,6 +47,18 @@ logger = logging.getLogger(__name__)
 max_tries = 60 * 1  # 1 minutes
 wait_seconds = 1
 
+from db.init_db import init_db
+
+def init() -> None:
+    db = SessionLocal()
+    init_db(db)
+
+
+def maininit() -> None:
+    logger.info("Creating initial data")
+    init()
+    logger.info("Initial data created")
+
 
 @retry(
     stop=stop_after_attempt(max_tries),
@@ -71,6 +83,8 @@ def main() -> None:
 
 print("Running Database Initialization")
 main()
+print("Creating super User")
+maininit()
 
 @app.get("/")
 def home():
