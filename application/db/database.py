a5690e87c304c161588f61e9423d2c6dcc6f4fc1
root <>
2023-01-10 11:50:39 +0300

    updated the db configs"

--- application/db/database.py
+++ application/db/database.py
@@ -1,31 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 import os
-import dotenv
 
+# load the environment variables
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
 
-# psql \
-#    --host=nsight.cftp73dgheul.us-east-1.rds.amazonaws.com \
-#    --port=5432 \
-#    --username=nsight \
-#    --dbname=nsight
-#    --password=neurallabs \
-
-
-dotenv.load_dotenv()
-
-
+# if not in production use sqlite
 if not os.getenv("PRODUCTION"):
     SQLALCHEMY_DATABASE_URL = "sqlite:///./neurallabs.db"
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-    )
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 else:
-    # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/nsight"
-    # SQLALCHEMY_DATABASE_URL = "sqlite:///./neurallabs.db"
-    SQLALCHEMY_DATABASE_URL = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
-    print(f"DB URL  {SQLALCHEMY_DATABASE_URL}")
-    engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+    SQLALCHEMY_DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=5, max_overflow=2, pool_pre_ping=True)
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
