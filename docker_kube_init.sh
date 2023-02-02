0aa5583770a057578279837d1c225be8c794d88c
root <>
2023-02-02 10:25:47 +0300

    setup is added

---
+++ docker_kube_init.sh
@@ -0,0 +1,9 @@
+
+
+# start minikube after installation..
+minikube start --extra-config=kubeadm.ignore-preflight-errors=NumCPU --force --cpus=1
+
+
+# force
+# dockers minikube --extra-config=kubeadm.ignore-preflight-errors=NumCPU --force --cpus=1 start
+# minikube start --vm-driver=docker --force
