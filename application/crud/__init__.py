af63a6670014bda52cb53af664750ceafbda167c
root <>
2023-02-10 17:04:30 +0300

    updated crud functions

--- application/crud/__init__.py
+++ application/crud/__init__.py
@@ -1,3 +1,5 @@
 from .crud_user import user
 from .crud_patient import patient
 from .crud_patient import report
+from .crud_patient import patient_delete
+from .crud_patient import report_delete
--- application/crud/crud_patient.py
+++ application/crud/crud_patient.py
@@ -5,10 +5,37 @@ from typing import Any, Dict, Optional, Union
 from sqlalchemy.orm import Session
 from datetime import datetime
 from crud.base import CRUDBase
-from model.inference import Patient, Report
+from model.inference import Patient, Report, DeleteReportObject, DeletePatientObject
 from schemas.patient import PatientCreate, PatientUpdate, ReportCreate, ReportUpdate
 
 
+
+
+
+class CRUDDeletePatient(CRUDBase[DeletePatientObject, PatientCreate, PatientUpdate]):
+    def create(self, db: Session, *, obj_in: PatientCreate) -> DeletePatientObject:
+        db_obj = DeletePatientObject(
+            id=obj_in.get('id'),
+            deleted_patient_id = obj_in.get('patient_id'),
+            deleted_at= obj_in.get('deleted_at')
+        )
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+
+class CRUDDeleteReport(CRUDBase[DeleteReportObject, PatientCreate, PatientUpdate]):
+    def create(self, db: Session, *, obj_in: PatientCreate) -> DeleteReportObject:
+        db_obj = DeleteReportObject(
+            id=obj_in.get('id'),
+            deleted_report_id = obj_in.get('report_id'),
+            deleted_at= obj_in.get('deleted_at')
+        )
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
 
 
 class CRUDPatient(CRUDBase[Patient, PatientCreate, PatientUpdate]):
@@ -25,7 +52,19 @@ class CRUDPatient(CRUDBase[Patient, PatientCreate, PatientUpdate]):
         return db_obj
 
     def get_by_user_id(self, db: Session, *, user_id: Any) -> Optional[Patient]:
-        return db.query(Patient).filter(Patient.user_id == user_id).all()
+        return db.query(Patient).filter(Patient.user_id == user_id, Patient.is_deleted==False).all()
+
+    def delete_update(
+        self, db: Session, *, db_obj: Report, obj_in: Union[ReportUpdate, Dict[str, Any]]
+    ) -> Patient:
+        if isinstance(obj_in, dict):
+            update_data = obj_in
+        else:
+            update_data = obj_in.dict(exclude_unset=True)
+        if update_data["is_deleted"]:
+            update_data['is_deleted'] = update_data["is_deleted"]
+        return super().update(db, db_obj=db_obj, obj_in=update_data)
+
 
 class CRUDReport(CRUDBase[Report, ReportCreate, ReportUpdate]):
     def create(self, db: Session, *, obj_in: ReportCreate) -> Report:
@@ -44,7 +83,7 @@ class CRUDReport(CRUDBase[Report, ReportCreate, ReportUpdate]):
 
     def update(
         self, db: Session, *, db_obj: Report, obj_in: Union[ReportUpdate, Dict[str, Any]]
-    ) -> Patient:
+    ) -> Report:
         if isinstance(obj_in, dict):
             update_data = obj_in
         else:
@@ -54,7 +93,20 @@ class CRUDReport(CRUDBase[Report, ReportCreate, ReportUpdate]):
             update_data['updated_at'] = datetime.now()
         return super().update(db, db_obj=db_obj, obj_in=update_data)
 
+    def delete_update(
+        self, db: Session, *, db_obj: Report, obj_in: Union[ReportUpdate, Dict[str, Any]]
+    ) -> Report:
+        if isinstance(obj_in, dict):
+            update_data = obj_in
+        else:
+            update_data = obj_in.dict(exclude_unset=True)
+        if update_data["is_deleted"]:
+            update_data['is_deleted'] = update_data["is_deleted"]
+        return super().update(db, db_obj=db_obj, obj_in=update_data)
+
 
 
 patient = CRUDPatient(Patient)
 report = CRUDReport(Report)
+patient_delete = CRUDDeletePatient(DeletePatientObject)
+report_delete = CRUDDeleteReport(DeleteReportObject)
