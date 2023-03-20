92a3b6007d2f4be63ae0c808fb774c1b2e90a339
Stephen-Kamau <stiveckamash@gmail.com>
2023-03-20 15:36:30 +0300

    removed k8s initialization file

--- docker_kube_init.sh
+++
@@ -1,9 +0,0 @@
-
-
-# start minikube after installation..
-minikube start --extra-config=kubeadm.ignore-preflight-errors=NumCPU --force --cpus=1
-
-
-# force
-# dockers minikube --extra-config=kubeadm.ignore-preflight-errors=NumCPU --force --cpus=1 start
-# minikube start --vm-driver=docker --force
