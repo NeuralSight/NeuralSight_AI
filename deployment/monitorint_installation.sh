7880b49b8f3da3d6f47b07bd2d12c0b0bfa8ffa4
Stephen-Kamau <stiveckamash@gmail.com>
2023-03-23 10:48:30 +0300

    added docker and kubectl installation

--- deployment/monitorint_installation.sh
+++ deployment/monitorint_installation.sh
@@ -1,4 +1,18 @@
 # first install kubectl, docker.
+
+
+# # Install Docker
+# apt-get update && apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+# add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io
+#
+# # Install Kubernetes
+# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+# echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+# apt-get update && apt-get install -y kubelet kubeadm kubectl
+
+
 # After that install helm then add the foolowing repos for monitoring
 
 
