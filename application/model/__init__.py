16fba8af995a2b84bc40773dfe8332e461376ddb
Stephen-Kamau <stiveckamash@gmail.com>
2023-07-09 23:12:28 +0300

    schema updates

--- application/model/__init__.py
+++ application/model/__init__.py
@@ -1,2 +1,2 @@
 from .user import User
-from .inference import Patient, Report
+from .inference import Patient, Report, OrthancModel
--- application/schemas/__init__.py
+++ application/schemas/__init__.py
@@ -1,4 +1,4 @@
 from .msg import Msg
 from .token import Token, TokenPayload
 from .user import User, UserCreate, UserInDB, UserUpdate
-from .patient import PatientCreate, PatientUpdate
+from .patient import PatientCreate, PatientUpdate,OrthancBase, OrthancCreate, OrthancBaseOptional
