db00029db282aaf835722c67c8c935dc968d42db
Stephen-Kamau <stiveckamash@gmail.com>
2023-07-23 00:24:28 +0300

    dependancy u[dated

--- application/endpoints/deps.py
+++ application/endpoints/deps.py
@@ -44,6 +44,28 @@ def get_current_user(
     return user
 
 
+def get_current_user_token(
+    db: Session = Depends(get_db), token: str = Depends(reusable_oauth2)
+) -> models.User:
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
+        )
+        token_data = schemas.TokenPayload(**payload)
+    # except (jwt.JWTError, ValidationError):
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+        )
+    user = crud.user.get(db, id=token_data.sub)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    user.token = token
+    return user
+
+
+
 def get_current_active_user(
     current_user: models.User = Depends(get_current_user),
 ) -> models.User:
